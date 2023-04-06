da31eea3356a22ae6e3c40a04cbdfaff5c40c290
Maise Dias <maise.sistemas@gmail.com>
2023-04-06 18:53:05 -0300

    first commit

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: "Essencia da Adoração",
+        artist: "Gabriela Rocha",
+        img: "img1",
+        audio: "music1"
+    },
+    {
+        name: "Meu Jesus",
+        artist: "Gabriela Rocha",
+        img: "img2",
+        audio: "music2"
+    },
+    {
+        name: "Me Atraiu",
+        artist: "Gabriela Rocha",
+        img: "img3",
+        audio: "music3"
+    },
+    {
+        name: "Anderson Freire",
+        artist: "Vai Tudo Bem",
+        img: "img4",
+        audio: "music4"
+    }
+]
